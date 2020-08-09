50936fc9e4f2aea32dfe8c7ca465d871be6540de
heumsi <heumsi@gmail.com>
2020-08-09 19:37:14 +0900

    ci/cd 테스트

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    """hello_world 를 출력합니다."""
-
-    return "Hello, world!"
+    return "Hello, world!!!!!!!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
